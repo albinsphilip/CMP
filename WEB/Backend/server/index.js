0429b0a1df5ac4dac1f88ca60d4b404928b6334f
AaronRM2003 <133229722+AaronRM2003@users.noreply.github.com>
2023-10-01 18:09:34 +0530

    Update index.js

--- WEB/Backend/server/index.js
+++ WEB/Backend/server/index.js
@@ -104,17 +104,23 @@ app.post('/login',  async(req, res) => {
             if (user) {
                 const token1 = jwt.sign({ username,exp:600 }, '1234');
                 var pos= user.pos
-                res.json({ token1,pos});
+                res.json({ token1,pos,user});
                 student=user;
                 if(pos==='teacher'){
                 sub=user.subject;
                 cls=user.class;
             }
+            const cursor1 = collection.find({ pos: { $in: ['teacher', 'student'] } });
+            tusers=[];
+            await cursor1.forEach(user => {
+              tusers.push(user);
+            });
              const cursor = collection.find({pos:'student',class: cls});
               users = [];
               await cursor.forEach(user => {
                 users.push(user);
               });
+            
             }
             else if (username === "" || password === "") {
                 res.status(401).json({ message: 'Enter username or password!' });
@@ -128,15 +134,203 @@ app.post('/login',  async(req, res) => {
   
 }); 
 
-app.get("/api/student", (req, res) => {
+app.get("/api/student", async(req, res) => {
 
     res.json(student);
 });
 var users = [];
+var tusers = [];
 app.get("/api/total", (req, res) => {
 
     res.json(users);
 });
+app.get("/api/alltotal", (req, res) => {
+
+  res.json(tusers);
+});
+
+app.post("/sprof",async(req,res)=>{
+   
+  const { roll,fname,mname,phone } = req.body;
+  try{
+
+    const db = client.db('data');
+    const collection = db.collection('user');
+    var myquery = {username:student.username,password:student.password}; 
+    var newvalues = { $set: {rollno:roll,fathersname:fname,mothersname:mname,phone:phone} };
+    await collection.updateOne(myquery, newvalues).then(res =>{
+      console.log("profile updated");
+     
+      
+
+  
+  }).catch(err =>{
+      console.log(err);
+  });
+  const user = await collection.findOne(myquery);
+  student=user;
+  
+}finally{
+
+}
+  res.json('updated');
+
+
+
+
+});
+app.post("/tprof",async(req,res)=>{
+   
+  const { subject,fname,mname,phone } = req.body;
+  try{
+
+    const db = client.db('data');
+    const collection = db.collection('user');
+    var myquery = {username:student.username,password:student.password}; 
+    var newvalues = { $set: {subject:subject,fathersname:fname,mothersname:mname,phone:phone} };
+    await collection.updateOne(myquery, newvalues).then(res =>{
+      console.log("profile updated");
+     
+      
+
+  
+  }).catch(err =>{
+      console.log(err);
+  });
+  const user = await collection.findOne(myquery);
+  student=user;
+  
+}finally{
+
+}
+  res.json('updated');
+
+
+
+
+});
+
+app.post("/removeDept",async(req,res)=>{
+  const arr = req.body;
+  const val = arr.arr;
+  console.log(val);
+  console.log(arr);
+  try{
+
+    const db = client.db('data');
+    const collection = db.collection('user');
+    if(val){
+    var myquery = {pos:'admin'}; 
+    var newvalues = { $pull: { ['depts.'+'dept']: val } }
+
+    await collection.updateOne(myquery, newvalues).then(res =>{
+      console.log("dept removed");
+      
+
+  
+  }).catch(err =>{
+      console.log(err);
+  });
+  }}
+  finally{
+
+  }
+  res.json('updated');
+});
+
+var dept='';
+app.post("/adddept",async(req,res) =>{
+  
+  const dept1  = req.body;
+  dept=dept1;
+  console.log(dept);
+  try{
+    const db = client.db('data');
+    const collection = db.collection('user');
+    var myquery = {pos:'admin',depts: { $exists: true, $type: 'array'}}; 
+    var newvalues = { $push: { ['depts'] :{$type:'array'}} };
+    await collection.updateOne(myquery, newvalues).then(res =>{
+      console.log("depts added");
+      
+
+  
+  }).catch(err =>{
+      console.log(err);
+  });
+
+  var myquery1 = {pos:'admin',['depts'+'.dept']: 'dept[0]' };
+  var newvalues1 = { $set: { ['depts'+ '.dept.$']: dept['d'] } };
+  
+  let result = await collection.updateOne(myquery1, newvalues1);
+             if (result.matchedCount === 0) {
+                myquery1 = {pos:'admin' };
+                newvalues1 = { $push: {  ['depts.'+'dept']: dept['d']} };
+              }
+               await   collection.updateOne(myquery1, newvalues1).then(res =>{
+                      console.log("dept updated");
+                      
+          
+                  }).catch(err =>{
+                      console.log(err);
+                  });
+                
+}finally{
+
+}
+  res.json('updated');
+});
+
+app.post("/adduser", async(req,res)=>{
+  const {username,pass,pos,name,item} = req.body;
+  
+  try{
+   
+
+    const db = client.db('data');
+    const collection = db.collection('user');
+    const user = await collection.findOne({ username, pass});
+    if(user){
+      res.status(401).json({ message: 'User already exists!' });
+    }
+    else{
+      try{
+        const us=[
+          {
+              "username": username,
+              "password": pass,
+              "name":name,
+              "pos":pos,
+              "dep":item
+                
+          }
+      ]
+      try {
+        const insertManyResult = await collection.insertMany(us);
+        const cursor1 = collection.find({ pos: { $in: ['teacher', 'student'] } });
+        tusers=[];
+        await cursor1.forEach(user => {
+          tusers.push(user);
+        });        
+        console.log(`${insertManyResult.insertedCount} documents successfully inserted.\n`);
+       
+      } catch (err) {
+        console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
+      }
+
+      }
+      finally{
+        
+      }
+
+    }
+
+  }
+  finally{
+    console.log("user created");
+  }
+ res.json({message:'success'});
+});
+
 app.post("/assig", async(req, res) => {
   const {assig,date} = req.body;
   try{
